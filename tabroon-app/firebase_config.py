c7f934ff63d2d263ae81cffa3ae59dbce9b76042
Alithechosenknight <115343530+Alithechosenknight@users.noreply.github.com>
2025-07-22 20:26:59 +0300

    Update firebase_config.py

--- tabroon-app/firebase_config.py
+++ tabroon-app/firebase_config.py
@@ -18,9 +18,23 @@ auth = firebase.auth()
 
 # Firebase Admin (for Firestore)
 service_account_info = st.secrets["firebase"]  # matches [firebase] in TOML
-cred = credentials.Certificate(dict(service_account_info))
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred, {
-        "projectId": service_account_info["project_id"]
-    })
-db = firestore.client()
+import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate({
+    "type": st.secrets.firebase.type,
+    "project_id": st.secrets.firebase.project_id,
+    "private_key_id": st.secrets.firebase.private_key_id,
+    "private_key": st.secrets.firebase.private_key,
+    "client_email": st.secrets.firebase.client_email,
+    "client_id": st.secrets.firebase.client_id,
+    "auth_uri": st.secrets.firebase.auth_uri,
+    "token_uri": st.secrets.firebase.token_uri,
+    "auth_provider_x509_cert_url": st.secrets.firebase.auth_provider_x509_cert_url,
+    "client_x509_cert_url": st.secrets.firebase.client_x509_cert_url,
+    "universe_domain": st.secrets.firebase.universe_domain,
+})
+
+firebase_admin.initialize_app(cred)
+
